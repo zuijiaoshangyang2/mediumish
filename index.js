ef332b2679436b6e800f6f1fb3fbc0f7323dafaa
yuniee <72388093+zuijiaoshangyang2@users.noreply.github.com>
2023-08-19 14:45:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = themeConfig => {
    * Default theme configuration
    */
   themeConfig = Object.assign(themeConfig, {
-    logo: themeConfig.logo || '/mylogo.png',
+    logo: themeConfig.logo || '/myicon.png',
     nav: themeConfig.nav || [
       {
         text: 'Blog',
